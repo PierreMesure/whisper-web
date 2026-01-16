3fb4b382ba223b7fd158b357db327ed3bd87da41
Pierre Mesure <pierre@mesu.re>
2026-01-16 21:26:13 +0100

    Upgrade dependencies and add tracking snippet only for my deployments

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,17 @@ import App from "./App";
 import "./css/index.css";
 import './i18n.ts'
 
+// Analytics
+const goatCounterUrl = import.meta.env.VITE_GOATCOUNTER_URL;
+if (goatCounterUrl && !document.getElementById("goatcounter-script")) {
+    const script = document.createElement("script");
+    script.id = "goatcounter-script";
+    script.dataset.goatcounter = goatCounterUrl;
+    script.async = true;
+    script.src = "//gc.zgo.at/count.js";
+    document.head.appendChild(script);
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <App />
